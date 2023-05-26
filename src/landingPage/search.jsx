0e061ee4333e7abeae968aa688db0f2519174abb
Rahul Tatineni <rtatineni3090@gmail.com>
2023-05-26 15:35:14 -0400

    working on to do list

---
+++ src/landingPage/search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './search.css'
+
+function SearchBar(){
+    return(
+    <form>
+        <input type="text"></input>
+    </form>)
+}
+
+export default SearchBar;
